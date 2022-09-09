c616f40f5791c45773dda1df9c24e23d8af6f271
Nana Agyemang-Duah <nana321.nad@gmail.com>
2022-09-09 21:24:12 +0200

    Updated database

---
+++ private/Model/Database.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace bcms
+{
+    public class Database
+    {
+        private static Database _instance;
+        private SqlConnection connection;
+
+        static Database()
+        {
+            _instance = new Database();
+
+        }
+
+        public Database Instance
+        {
+            get { return _instance; }
+        }
+
+        private string GetConnectionString()
+        {
+            string SQLConnection = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\bcms.mdf; Integrated Security = True";
+            return SQLConnection;
+        }
+
+        public bool connect()
+        {
+            this.connection = new SqlConnection(GetConnectionString());
+            try
+            {
+
+                connection.Open();
+                connection.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
